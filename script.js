509b2927e0ebdf15e450b48c64cc41d29a47baaf
Amanda Barsanti <barsanti@Amandas-MacBook-Pro.local>
2023-08-21 15:27:16 -0400

    added js file

---
+++ script.js
@@ -0,0 +1,27 @@
+let displayDate= document.getElementById("currentDay");
+  displayDate.innerHTML = NowMoment;
+  let currentHour = moment ().format ("HH");
+
+  $(".time-div").each(function () {
+    var timeDiv= $(this).attr("id").split("-")[1];
+   
+    if (currentHour == timeDiv) {
+      $(this).addClass ("present");
+      $(this).children (".description").addClass("present");
+
+    } else if (currentHour < timeDiv) {
+      $(this).removeClass ("present");
+      $(this).addClass ("future");
+
+    } else if (currentHour > timeDiv) {
+      $(this).removeClass ("future");
+      $(this).addClass ("past");
+    }
+  });
+
+  $(".saveBtn").click(function (event) {
+    event.preventDefault ();
+    var value =$(this).siblings(".time-block").val ();
+    var time =$(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
